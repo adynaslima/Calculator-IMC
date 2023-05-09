3df668f6ecd0877812fbca6fc8c8321544d0a371
Adyna Lima <adynalsl@gmail.com>
2023-05-09 15:12:57 -0300

    :construction:feat: criando condições.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,4 +12,21 @@ form.addEventListener('submit', function(event) {
     let description = '';
 
     document.querySelector('#infos').classList.remove('hidden');
+
+    if (bmi < 18.5) {
+        description = 'Cuidado! O seu peso está abaixo do ideal!';
+    } else if (bmi >= 18.5 && bmi <= 25) {
+        description = 'Parabéns! Você está com peso ideal!';
+    } else if (bmi > 25 && bmi <= 30) {
+        description = 'Cuidado! Você está com sobrepeso!';
+    } else if (bmi > 30 && bmi <= 35) {
+        description = 'Cuidado! Você está com obesidade moderada!';
+    } else if (bmi > 35 && bmi <= 40) {
+        description = 'Cuidado! Você está com obesidade severa!'; 
+    } else {
+        description = 'Cuidado! Você está com obesidade mórbida!';
+    }
+
+    value.textContent = bmi.replace('.', ',');
+    document.querySelector('#description').textContent = description; 
 });
